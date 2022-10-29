4000ab568d65a129fce79323e7198f929db12bc9
Saveli Savich <Saveli_Savich@epam.com>
2022-10-30 00:14:01 +0300

    add footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RoutePath } from 'src/app/app-routing.module';
 
 @Component({
 	selector: 'app-footer',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
 	styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-	constructor() {}
+	public routes: typeof RoutePath = RoutePath;
+
+	constructor(private iconRegistry: MatIconRegistry, private sanitizer: DomSanitizer) {
+		this.iconRegistry.addSvgIcon(
+			'booking',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/svg/long_up_right.svg')
+		);
+
+		this.iconRegistry.addSvgIcon('facebook', this.sanitizer.bypassSecurityTrustResourceUrl('assets/svg/facebook.svg'));
+
+		this.iconRegistry.addSvgIcon('twitter', this.sanitizer.bypassSecurityTrustResourceUrl('assets/svg/twitter.svg'));
+
+		this.iconRegistry.addSvgIcon(
+			'instagram',
+			this.sanitizer.bypassSecurityTrustResourceUrl('assets/svg/instagram.svg')
+		);
+	}
 }
